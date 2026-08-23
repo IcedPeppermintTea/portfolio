cfb223802671a52b3e733af1bc20830466a66260
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-08-23 14:40:42 -0700

    wip: slider data populating

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
+import { projects } from "../data/selected-projects";
+
 function Slider() {
+  const [current, setCurrent] = useState(0);
+
   return (
     <section>
       <div className="selected-projects">
         <div className="header">
-          <h1>Selected Projects</h1>
+          <h2>Selected Projects</h2>
           <p>Some of my most recent projects</p>
         </div>
         <div className="carousel-arrows">
           <span></span>
+        </div>
+        <div className="carousel-slider">
+          {projects.map((project, projectIdx) => (
+            <div key={projectIdx} className="card">
+              <div className="card-info">
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <div className="card-info-stack">
+                  {project.stack.map((value, stackId) => (
+                    <div key={stackId}>{value}</div>
+                  ))}
+                </div>
+                <div className="card-info-links">
+                  <a href={project.links.live}>live demo</a>
+                  <a href={project.links.readme}>source</a>
+                </div>
+              </div>
+              <div className="card-image">
+                <img src={project.images[0].src} alt={project.images[0].alt} />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
